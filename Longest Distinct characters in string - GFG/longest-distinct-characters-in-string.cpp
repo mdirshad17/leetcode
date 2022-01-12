55b0f9331b58d676c2c36b1907371ac7eee5af8c
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-01-12 13:51:25 +0530

    Added solution - LeetHub

--- Longest Distinct characters in string - GFG/longest-distinct-characters-in-string.cpp
+++ Longest Distinct characters in string - GFG/longest-distinct-characters-in-string.cpp
@@ -16,38 +16,22 @@ int main()
 // Contributed By: Pranay Bansal
 // } Driver Code Ends
 
-int check(int * a){
-    for(int i=0;i<26;i++){
-        if(a[i]>1) return 0;
-    }
-    return 1;
-}
-int good(int m,string & s,int n){
-    int a[26]={0};
-    for(int i=0;i<m;i++){
-        a[s[i]-'a']++;
-    }
-    if(check(a)) return 1;
-    for(int i=m;i<n;i++){
-        a[s[i]-'a']++;
-        a[s[i-m]-'a']--;
-        if(check(a)) return 1;
-    } 
-   
-    return 0;    
-}
+
 
 int longestSubstrDistinctChars (string s)
 {
     int n=s.size();
     int ans=0;
-     int l=1,r=n;
-     while(l<=r){
-         int m=(l+r)/2;
-         if(good(m,s,n)){
-             ans=m;
-             l=m+1;
-         }else r=m-1;
-     }
-     return ans;
+    int j=0;
+    int a[26]={0};
+    for(int i=0;i<n;i++){
+        a[s[i]-'a']++;
+        while(a[s[i]-'a']>1){
+            a[s[j]-'a']--;
+            j++;
+        }
+        int len=i-j+1;
+        ans=max(ans,len);
+    }
+    return ans;
 }
