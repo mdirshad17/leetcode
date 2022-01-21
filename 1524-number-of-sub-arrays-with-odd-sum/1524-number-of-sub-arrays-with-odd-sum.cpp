75c99645105077c86374760241172ff85c368205
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-01-21 18:05:42 +0530

    Time: 324 ms (7.83%), Space: 108.1 MB (35.81%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -7,19 +7,18 @@ public:
         for(int i=0;i<n;i++){
             arr[i]=arr[i]%2;
         }
-        map<int,ll> mp;
-        mp[0]=1;
+        ll a[2];
+        memset(a,0,sizeof(a));
+        // mp[0]=1;
+        a[0]=1;
         ll ans=0;
         int sum=0;
         for(int i=0;i<n;i++){
             sum+=arr[i];
             sum=sum%2;
             int ss=(sum+1)%2;
-            if(mp.count(ss)){
-                ans+=mp[ss];
-                ans=ans%M;
-            }
-            mp[sum]++;
+            (ans+=a[ss])%=M;
+            a[sum]=(a[sum]+1)%M;
         }
         return ans;
     }
