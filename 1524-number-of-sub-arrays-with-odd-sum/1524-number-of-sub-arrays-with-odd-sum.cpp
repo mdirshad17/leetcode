1c2db12c4899944b511d7a7266dc494f9634faec
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-01-21 18:03:16 +0530

    Time: 342 ms (5.48%), Space: 108.2 MB (35.81%) - LeetHub

---
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    #define ll long long
+    #define M 1000000007
+    int numOfSubarrays(vector<int>& arr) {
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            arr[i]=arr[i]%2;
+        }
+        unordered_map<int,ll> mp;
+        mp[0]=1;
+        ll ans=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            sum=sum%2;
+            int ss=(sum+1)%2;
+            if(mp.count(ss)){
+                ans+=mp[ss];
+                ans=ans%M;
+            }
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
