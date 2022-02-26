e68f69820da7f5471e28f3acd54f28816ffef08c
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-02-26 13:39:22 +0530

    Time: 4 ms (24.72%), Space: 5.8 MB (90.18%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,21 +1,13 @@
 class Solution {
-public:
-    int dp[60];
-    int solve(int n){
-        // cout<<n<<endl;
-        if(n==0||n==1)return 1;
-        if(dp[n]!=-1)return dp[n];
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            int l=i*solve(n-i);
-            ans=max(ans,l);
-        }
-        return dp[n]=ans;
-        
-    }
+public: 
     int integerBreak(int n) {
-        memset(dp,-1,sizeof(dp));
-        if(n==3||n==2)return (n-1);
-        return solve(n);
+        if(n==3||n==2)return n-1;
+        int num=0;
+        if((n%3)==0)num=1;
+        else if((n%3)==1)num=4,n-=4;
+        else num=2,n-=2;
+        for(int i=1;i<=n/3;i++)num=num*3;
+        return num;
+        
     }
 };
