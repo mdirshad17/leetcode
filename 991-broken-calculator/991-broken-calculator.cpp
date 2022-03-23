127fa49749e8d8d0e46bcff4ae40994c58f2e5f8
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-03-23 07:45:48 +0530

    Time: 5 ms (16.41%), Space: 5.9 MB (30.21%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
+    int solve(int s,int e){
+        if(s==e)return 0;
+        if(s<e)return (e-s);
+        if((s&1))return 1+solve(s+1,e);
+        return 1+solve(s/2,e);
+    }
     int brokenCalc(int startValue, int target) {
-          if(startValue>=target) return startValue-target;
-          if((target&1)==0) return 1+brokenCalc(startValue,target/2);
-        
-          return 1+brokenCalc(startValue,target+1);
+        return solve(target,startValue);
     }
 };
