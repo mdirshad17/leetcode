4b4c150cbedd4c0977c958eaba668097e117e46a
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-04-23 09:04:00 +0530

    Time: 5 ms (48.05%), Space: 7 MB (57.22%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -3,18 +3,31 @@ public:
 
     // Encodes a URL to a shortened URL.
     map<string,string> mp;
-    map<string,string> mpx;
+    // map<string,string> mpx;
+    #define ll long long 
+
+    #define M 999999900000001
+    #define M1 1000000007
     int num=0;
-    string encode(string longUrl) {
-        mp[longUrl]=to_string(num);
-        mpx[mp[longUrl]]=longUrl;
-        num++;
-        return mp[longUrl];
+    string encode(string s) {
+        ll sum=0;
+        ll sum1=0;
+        
+        for(int i=0;i<s.size();i++){
+            sum=sum*97LL+(s[i]-' ');
+            sum=sum%M;
+            sum1=sum1*97LL+(s[i]-' ');
+            sum1=sum%M1;
+        }
+        string g=to_string(sum)+'/'+to_string(sum1);
+        mp[g]=s;
+        return g;
+        
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return mpx[shortUrl];
+        return mp[shortUrl];
     }
 };
 
