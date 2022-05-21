f959693db909865a23e85a7c48a39f8e657b167a
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-05-21 13:53:01 +0530

    Time: 113 ms (52.85%), Space: 11.8 MB (100.00%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -2,18 +2,20 @@ class Solution {
 public:
     int dx[4]={0,0,1,-1};
     int dy[4]={1,-1,0,0};
+    #define pii pair<int,int>
+    
     int minCost(vector<vector<int>>& grid) {
            int n=grid.size();
            int m=grid[0].size();
            int dp[n][m];
            memset(dp,1,sizeof(dp));
            
-           deque<pair<int,int>> dq;
-           dq.push_front({0,0});
+           priority_queue<pii,vector<pii>,greater<pii>> pq;
+           pq.push({0,0});
            dp[0][0]=0;
-           while(dq.size()){
-               auto tp=dq.front();
-               dq.pop_front();
+           while(pq.size()){
+               auto tp=pq.top();
+               pq.pop();
                int l=grid[tp.first][tp.second]-1;
                for(int i=0;i<4;i++){
                    int wt=(l!=i);
@@ -21,8 +23,8 @@ public:
                    int Y=tp.second+dy[i];
                    if(X>=0 && X<n && Y>=0 && Y<m && dp[X][Y]>wt+dp[tp.first][tp.second]){
                        dp[X][Y]=wt+dp[tp.first][tp.second];
-                       if(wt==0)dq.push_front({X,Y});
-                       else dq.push_back({X,Y});
+                       pq.push({X,Y});
+                     
                    }
                }
            }
