a4a98bb0b7c4a6f08e074edd6e120257cccee027
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-06-21 15:20:58 +0530

    Time: 118 ms (11.03%), Space: 8 MB (11.27%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -1,60 +1,49 @@
 class Solution
 {
     public:
-    #define ll long long
-    
-    unordered_map<char,int> mpx,mpg;
-    int enough_extra(int extra){
-        int l=min(mpg['Q'],mpx['Q']);
-        int m=min(mpg['W'],mpx['W']);
-        int n=min(mpg['E'],mpx['E']);
-        int o=min(mpg['R'],mpx['R']);
-        // cout<<l<<" "<<m<<" "<<n<<" "<<o<<" "<<extra<<endl;
-        return ((l+m+n+o)>=extra);
+#define ll long long
+
+    unordered_map<char, int> mpx, mpg;
+    int enough_extra(int extra)
+    {
+        int l = min(mpg['Q'], mpx['Q']);
+        int m = min(mpg['W'], mpx['W']);
+        int n = min(mpg['E'], mpx['E']);
+        int o = min(mpg['R'], mpx['R']);
+
+        return ((l + m + n + o) >= extra);
     }
-    int good(string & s,int m,int extra){
-        int n=s.size();
-        mpg['Q']=0;
-        mpg['W']=0;
-        mpg['E']=0;
-        mpg['R']=0;
-        for(int i=0;i<m;i++){
-            mpg[s[i]]++;
-        }
-        if(enough_extra(extra))return 1;
-        // cout<<"HELLO WORLD\n";
-        for(int i=m;i<n;i++){
-            mpg[s[i]]++;
-            mpg[s[i-m]]--;
-            if(enough_extra(extra))return 1;
-        }
-        return 0;
-    }
+
     int balancedString(string s)
     {
         int n = s.size();
-        int ans=n;
-        unordered_map<char,int> mp;
-        for(char c:s){
+        int ans = n;
+        unordered_map<char, int> mp;
+        for (char c: s)
+        {
             mp[c]++;
         }
-        int extra=0;
-        for(auto x:mp){
-            if(x.second>n/4){
-                mpx[x.first]=(x.second-n/4);
-                extra+=(x.second-n/4);
+        int extra = 0;
+        for (auto x: mp)
+        {
+            if (x.second > n / 4)
+            {
+                mpx[x.first] = (x.second - n / 4);
+                extra += (x.second - n / 4);
             }
         }
-        // cout<<extra<<endl;
-        if(extra<=1)return extra;
-        int l=1,r=n;
-        // int ans=n;
-        while(l<=r){
-            int m=(l+r)/2;
-            if(good(s,m,extra)){
-                ans=m;
-                r=m-1;
-            }else l=m+1;
+       	// cout<<extra<<endl;
+        if (extra <= 1) return extra;
+        int j = 0;
+        for (int i = 0; i < n; i++)
+        {
+            mpg[s[i]]++;
+            while (enough_extra(extra))
+            {
+                ans = min(ans, i - j + 1);
+                mpg[s[j]]--;
+                j++;
+            }
         }
         return ans;
     }
