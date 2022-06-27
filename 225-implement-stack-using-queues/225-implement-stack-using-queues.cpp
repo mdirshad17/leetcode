f3fc68cdaf9ea1be2a7d429ba301ca490ede1d05
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-06-27 17:17:07 +0530

    Time: 4 ms (25.83%), Space: 7 MB (17.38%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -1,50 +1,71 @@
-class MyStack {
-public:
-    queue<int> q1;    
+class MyStack
+{
+    public:
+        queue<int> q1;
+     queue<int> q2;
     MyStack() {
         
     }
-    
-    void push(int x) {
+
+    void push(int x)
+    {
         q1.push(x);
+        // cout<<q1.size()<<endl;
     }
-    
-    int pop() {
-        queue<int> q;
-        while(q1.size()>1){
-            q.push(q1.front());
+
+    int pop()
+    {
+        // cout<<q1.size()<<endl;
+        while (q1.size() > 1)
+        {
+            // cout<<q1.front()<<" ";
+            q2.push(q1.front());
             q1.pop();
         }
-        int d=q1.front();
+        int d = q1.front();
         q1.pop();
-        q1=q;
+        while(q2.size()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        
         return d;
     }
-    
-    int top() {
-        queue<int> q;
-        while(q1.size()>1){
-            q.push(q1.front());
+
+    int top()
+    {
+        
+        while (q1.size() > 1)
+        {
+            q2.push(q1.front());
             q1.pop();
         }
-        int d=q1.front();
-        q.push(q1.front());
+        int d = q1.front();
         q1.pop();
-        q1=q;
-        return d;  
+         while(q2.size()){
+            q1.push(q2.front());
+            q2.pop();
+        }
+        
+        q1.push(d);
+        
+        return d;
     }
-    
-    bool empty() {
-        // cout<<q1.size();
-        return ((q1.size()==0));
+
+    bool empty()
+    {
+        cout<<q1.size()<<endl;
+        if(q1.size()==0)return 1;
+        return 0;
+        
     }
 };
 
 /**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
+ *Your MyStack object will be instantiated and called as such:
+ *MyStack* obj = new MyStack();
+ *obj->push(x);
+ *int param_2 = obj->pop();
+ *int param_3 = obj->top();
+ *bool param_4 = obj->empty();
  */
