5e86a027c8911bef1717e5a40f5c09d92cd6d9cf
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-06-28 10:50:37 +0530

    Time: 56 ms (89.38%), Space: 17.4 MB (37.46%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -4,50 +4,31 @@ class Solution
         int minDeletions(string s)
         {
             int n = s.size();
-            int a[26];
-            memset(a, 0, sizeof(a));
+            int cnt[n+1];
+            int a[26]{};
+            memset(cnt, 0, sizeof(cnt));
+            
             for (int i = 0; i < n; i++)
             {
                 a[s[i] - 'a']++;
             }
-            sort(a, a + 26);
-            int ans = 0;
-            while (1)
+            int ans=0;
+            for (int i = 0; i < 26; i++)
             {
-                int cnt = 0;
-                int f = 0;
-                for (int i = 0; i < 26; i++)
+                if (a[i] == 0) continue;
+                int ab = 0;
+                for (int j = a[i]; j >= 0; j--)
                 {
-                   	// cout<<a[i]<<" ";
-                    if (a[i] == 0) continue;
-                    if (i != 0 && a[i] == a[i - 1])
+                    if (cnt[j] == 0)
                     {
-                        cnt++;
-                    }
-                    else
-                    {
-                        int add = max(cnt - 1, 0);
-                       	// cout<<add<<" ";
-                        if (cnt > 1) f = 1;
-                        ans += add;
-                        cnt = 1;
+                        ab = j;
+                        break;
                     }
                 }
-                int add = max(cnt - 1, 0);
-               	// cout<<add<<" ";
-                if (cnt > 1) f = 1;
-                ans += add;
-                for (int i = 0; i < 26; i++)
-                {
-                    if (a[i] == 0) continue;
-                    if (i != 0 && a[i] == a[i - 1])
-                    {
-                        a[i - 1] -= 1;
-                    }
-                }
-                sort(a, a + 26);
-                if (f == 0) break;
+                cnt[ab]=1;
+                ans+=(a[i]-ab);
             }
             return ans;
+            
         }
 };
