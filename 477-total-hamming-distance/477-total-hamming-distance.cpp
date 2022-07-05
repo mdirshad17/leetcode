9522ddcbc984356e91448d507ca47c675a7659c0
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-07-05 16:42:28 +0530

    Time: 92 ms (32.01%), Space: 19 MB (55.80%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.cpp
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -3,27 +3,24 @@ class Solution
     public:
         int totalHammingDistance(vector<int> &nums)
         {
-            int arr[32];
+            int ans = 0;
+            
             int n = nums.size();
-            memset(arr, 0, sizeof(arr));
             for (int i = 0; i <= 31; i++)
             {
                 int num = (1 << i);
+                int cnt=0;
                 for (int j = 0; j < n; j++)
                 {
                     if ((num &nums[j]))
                     {
-                        arr[i]++;
+                       cnt++;
                     }
                 }
-            }
-            int ans = 0;
-            for (int i = 0; i <= 31; i++)
-            {
-                int setbit = arr[i];
-                int unsetbit = n - arr[i];
-                ans = ans + (setbit *unsetbit);
+                int unset=n-cnt;
+                ans+=cnt*unset;
             }
             return ans;
+            
         }
 };
