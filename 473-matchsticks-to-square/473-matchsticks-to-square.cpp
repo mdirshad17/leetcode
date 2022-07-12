f3d4059bb6524c195727a5f2a0e8617bf4dfe268
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-07-12 07:56:54 +0530

    Time: 1292 ms (8.31%), Space: 13.5 MB (23.00%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -49,27 +49,27 @@ class Solution
         M = (1 << n) - 1;
         generate(0, n, m, 0, 0);
         int l = v.size();
-        if (l <= 200)
-        {
-            for (int i = 0; i < l; i++)
-            {
-                for (int j = i + 1; j < l; j++)
-                {
-                    for (int k = j + 1; k < l; k++)
-                    {
-                        for (int o = k + 1; o < l; o++)
-                        {
-                            if ((v[i] &v[j]) || v[i] &v[k] || v[i] &v[o] || v[j] &v[k] || v[j] &v[o] || v[k] &v[o]){
-                                continue;
-                            }
-                            int mt=v[i]|v[j]|v[k]|v[o];
-                            if(mt==M)return 1;
-                        }
-                    }
-                }
-            }
-            return 0;
-        }
+        // if (l <= 200)
+        // {
+        //     for (int i = 0; i < l; i++)
+        //     {
+        //         for (int j = i + 1; j < l; j++)
+        //         {
+        //             for (int k = j + 1; k < l; k++)
+        //             {
+        //                 for (int o = k + 1; o < l; o++)
+        //                 {
+        //                     if ((v[i] &v[j]) || v[i] &v[k] || v[i] &v[o] || v[j] &v[k] || v[j] &v[o] || v[k] &v[o]){
+        //                         continue;
+        //                     }
+        //                     int mt=v[i]|v[j]|v[k]|v[o];
+        //                     if(mt==M)return 1;
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     return 0;
+        // }
        	// for (int i = 0; i < (1 << n); i++)
        	// {
        	//     for (int j = 0; j < 5; j++)
