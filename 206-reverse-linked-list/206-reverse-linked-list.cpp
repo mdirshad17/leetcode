c44aee3c69fca497436463dd7547eda0b04c85b1
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-07-18 16:38:01 +0530

    Time: 5 ms (86.75%), Space: 8.5 MB (20.32%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -1,38 +1,40 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    #define Node ListNode
-    void print(Node * head){
-        while(head!=NULL){
-            cout<<head->val<<" ";
-            head=head->next;
+class Solution
+{
+    public:
+#define Node ListNode
+    void print(Node *head)
+    {
+        while (head != NULL)
+        {
+            cout << head->val << " ";
+            head = head->next;
         }
     }
-    // RP stands for reversedPart
-    Node * reverseLL(Node * head){
-        if(head==NULL||head->next==NULL)return head;
-        Node * RP=reverseLL(head->next);
-        Node * temp=RP;
-        while(temp->next!=NULL){
-            temp=temp->next;
-        }
-        temp->next=head;
-        head->next=NULL;
+   	// RP stands for reversedPart
+    // LN stands for LastNode
+    Node* reverseLL(Node *head)// O(N)
+    {
+        if (head == NULL || head->next == NULL) return head;
+        Node *RP = reverseLL(head->next);
+        Node *LN = head->next;
+        LN->next = head;
+        head->next = NULL;
         return RP;
-        
     }
-    ListNode* reverseList(ListNode* head) {
-        if(head==NULL || head->next==NULL) return head;
-        Node * res=reverseLL(head);
+    ListNode* reverseList(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL) return head;
+        Node *res = reverseLL(head);
         return res;
     }
 };
