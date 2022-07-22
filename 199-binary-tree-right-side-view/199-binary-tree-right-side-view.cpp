e7fdafaf64be3504727462153c965d9e7734ec63
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-07-22 16:30:12 +0530

    Time: 7 ms (45.86%), Space: 12 MB (67.08%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -1,33 +1,34 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        int maxlevel = 1;
-    vector<int> res;
-    void solve(TreeNode *root, int level)
-    {
-        if (!root) return;
-        if (level== maxlevel)
-        {
-            res.push_back(root->val);
-            maxlevel++;
-        }
-        solve(root->right, level + 1);
-        solve(root->left, level + 1);
+class Solution {
+public:
+    #define Node TreeNode
+    void PO(Node * node,int &maxlevel,int nodelevel,vector<int> & res){
+    if(node==NULL)return;
+    if(maxlevel<nodelevel){
+        res.push_back(node->val);
+        maxlevel++;
     }
-    vector<int> rightSideView(TreeNode *root)
-    {
-        solve(root, 1);
+    swap(node->left,node->right);
+    PO(node->left,maxlevel,nodelevel+1,res);
+    PO(node->right,maxlevel,nodelevel+1,res);
+    
+}
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        if(root==NULL)return res;
+        int maxlevel=0;
+        int nodelevel=1;
+        PO(root,maxlevel,nodelevel,res);
         return res;
     }
 };
