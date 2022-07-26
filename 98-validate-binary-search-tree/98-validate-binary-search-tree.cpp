9b25db3efc5c0ec1b8169c068504fd189a986692
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-07-26 17:53:58 +0530

    Time: 12 ms (86.35%), Space: 21.8 MB (30.51%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,38 +1,31 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        long long prev = LONG_MIN;
-    int solve(TreeNode *root)
-    {
-        if (!root)
-        {
-            return 1;
-        }
-        int lp = solve(root->left);
-        if (lp == 0) return 0;
-        if (prev >=root->val)
-        {
-            return 0;
-        }
-        prev = root->val;
-
-        int rp = solve(root->right);
-        if (rp == 0) return 0;
-        return 1;
+class Solution {
+public:
+    #define Node TreeNode
+    int solve(Node * root,long long leftrange,long long rightrange){
+        if(root==NULL)return true;
+        // if(leftrange<=root->val && rightrange>=root->val)
+        if(leftrange>root->val||rightrange<root->val)return 0;
+        
+        int lp=solve(root->left,leftrange,1LL*root->val-1);
+        int rp=solve(root->right,1LL*root->val+1,rightrange);
+        if(lp && rp)return 1;
+        return 0;
     }
-    bool isValidBST(TreeNode *root)
-    {
-        return solve(root);
+    bool isValidBST(TreeNode* root) {
+        long long leftrange=LONG_MIN;
+        long long rightrange=LONG_MAX;
+        int f=solve(root,leftrange,rightrange);
+        return f;
     }
 };
