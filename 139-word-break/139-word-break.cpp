51a472f5081e64957071df1b8f950b7c1a909d46
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-08-06 23:22:07 +0530

    Time: 16 ms (57.44%), Space: 8.3 MB (76.34%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    map<string,int> mp;
+    int dp[305];
+    int solve(int i,int n,string &s){
+        // ......base case
+        if(i>=n)return 1;
+        if(dp[i]!=-1)return dp[i];
+        int ans=0;
+        string g;
+        for(int j=i;j<n;j++){
+            g.push_back(s[j]);
+            if(mp.count(g)){
+                int l=solve(j+1,n,s);
+                if(l==1){
+                    ans=1;
+                }
+            }
+        }
+        return dp[i]=ans;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        memset(dp,-1,sizeof(dp));
+        for(auto x:wordDict){
+            mp[x]=1;
+        }
+        int n=s.size();
+        int ans=solve(0,n,s);
+        return ans;
+        
+    }
+};
