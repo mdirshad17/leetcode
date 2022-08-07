91dc31de5e9cbb73f0833803aef9279218d6e78a
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-08-07 21:42:30 +0530

    Time: 82 ms (82.35%), Space: 8 MB (26.25%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,21 +1,39 @@
-class Solution {
-public:
-    int used[16];
-    int solve(int s,int n){
-        if(s>n){
-            return 1;
-        }
-        int ans=0;
-        for(int i=1;i<=n;i++){
-            if(!used[i] && (((i%s)==0) || ((s%i)==0))){
-                used[i]=1;
-                ans+=solve(s+1,n);
-                used[i]=0;
+class Solution
+{
+    public:
+    int dp[15][1<<15];
+    int cnt=0;
+        int solve(int i, int n, int mask)
+        {
+
+           	//... base case
+            
+            if(i>=n){
+                return 1;
             }
+            // if(dp[i][mask]!=-1)return dp[i][mask];    
+           	// follow the constraint of the problem
+           	// constraint to be a permutation
+            cnt++;
+            int ans = 0;
+        
+            for (int j = 1; j <= n; j++)
+            {
+                if ((mask &(1 << (j - 1))) != 0) continue;
+                
+                if ((j % (i + 1)) == 0 || ((i + 1) % j) == 0)
+                {
+                    ans += solve(i + 1, n, (mask | (1 << (j - 1))));
+                }
+            }
+            return dp[i][mask]=ans;
         }
+    int countArrangement(int n)
+    {
+        memset(dp,-1,sizeof(dp));
+
+        int ans = solve(0, n, 0);
+        cout<<cnt<<endl;
         return ans;
-    }
-    int countArrangement(int n) {
-        return solve(1,n);
     }
 };
