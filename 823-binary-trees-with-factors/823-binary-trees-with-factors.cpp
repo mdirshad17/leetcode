901ba90a6b5fe24839cd93ce372f424fb453cde7
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-08-09 10:58:43 +0530

    Time: 142 ms (36.00%), Space: 9.1 MB (81.85%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    map<int,int> mp;
     #define ll long long 
-    #define M 1000000007 
-    int n;
+    #define M 1000000007
+    unordered_map<int,int> mp;
     ll dp[1005];
-    ll solve(int p,vector<int> &v){
-        if(dp[p]!=-1)return dp[p];
-        ll ans=1;
-        for(int i=0;i<n;i++){
-            if((v[p]%v[i]))continue;
-            int req=v[p]/v[i];
-            if(mp.count(req)){
-                ans+=(solve(i,v)*1LL*solve(mp[req],v)*1LL)%M;
-                ans=ans%M;
+ll solve(int ind,vector<int> & arr){
+        if(dp[ind]!=-1)return dp[ind];
+        int ans=1;
+        for(int i=0;i<arr.size();i++){
+            if(arr[ind]%arr[i])continue;
+            int l=arr[ind]/arr[i];
+            if(mp.count(l)){
+                ans+=((solve(i,arr)%M)*(solve(mp[l],arr)%M))%M;
             }
+            ans=ans%M;
         }
-        return dp[p]=ans;
-    }    
+        return dp[ind]=ans;
+    }
     int numFactoredBinaryTrees(vector<int>& arr) {
-        memset(dp,-1,sizeof(dp));
-        n=arr.size();
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<n;i++){
+        ll ans=0;
+        for(int i=0;i<arr.size();i++){
             mp[arr[i]]=i;
         }
-        ll ans=0;
-        for(int i=0;i<n;i++){
-            dp[i]=solve(i,arr);
-            ans+=dp[i];
+        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<arr.size();i++){
+            ans+=solve(i,arr);
             ans=ans%M;
         }
         return ans;
