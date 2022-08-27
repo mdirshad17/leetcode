1c10d2f63a65d7ae8331b9d43538b5ca668b4b5a
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2022-08-27 11:15:52 +0530

    Time: 2086 ms (25.44%), Space: 296.7 MB (12.63%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,37 +1,42 @@
-class Solution {
-public:
-    set<int> st;
-    int subarraysum(vector<int> & v,int k){
-        int ans=INT_MIN;
-        st.clear();
-        st.insert(0);
-        st.insert(INT_MAX/10);
-        int sum=0;
-        for(int x:v){
-            sum+=x;
-            auto it=st.lower_bound(sum-k);
-            int val=(*it);
-            int sumh=sum-val;
-            ans=max(ans,sumh);
-            st.insert(sum);
-        }
-        return ans;
-        
-    }
-    int maxSumSubmatrix(vector<vector<int>>& matrix, int K) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int ans=INT_MIN;
-        for(int i=0;i<n;i++){
-            vector<int> b(m,0);
-            for(int j=i;j<n;j++){
-                for(int k=0;k<m;k++){
-                    b[k]+=matrix[j][k];
-                }
-                int cnt=subarraysum(b,K);
-                ans=max(cnt,ans);
+class Solution
+{
+    public:
+        int process(vector<int> & b,int k){
+            int n=b.size();
+            int ans=INT_MIN;
+            set<int> st;
+            st.insert(INT_MAX/10);
+            st.insert(0);
+            int sum=0;
+            for(int i=0;i<n;i++){
+                sum+=b[i];
+                int sv=(sum-k);
+                auto it=st.lower_bound(sv);
+                int sumh=sum-(*it);
+                ans=max(ans,sumh);
+                st.insert(sum);
             }
+            return ans;
         }
-        return ans;
-    }
+        int maxSumSubmatrix(vector<vector < int>> &matrix, int k)
+        {
+
+            int n = matrix.size();
+            int m = matrix[0].size();
+            int ans = INT_MIN;
+            for (int i = 0; i < n; i++)
+            {
+                vector<int> b(m, 0);
+                for (int j = i; j < n; j++)
+                {
+                    for (int k = 0; k < m; k++)
+                    {
+                        b[k] += matrix[j][k];
+                    }
+                    int l = process(b, k);
+                    ans = max(ans, l);
+                }
+            }
+            return ans;
+        }
 };
