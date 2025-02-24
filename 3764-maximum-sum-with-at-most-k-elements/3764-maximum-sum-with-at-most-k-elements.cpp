c0c51f045f1cfb3ab5473e6564e505309e46ff80
mdirshad17 <66658713+mdirshad17@users.noreply.github.com>
2025-02-24 23:36:09 +0530

    Time: 142 ms (94.74%), Space: 97.8 MB (100%) - LeetHub

---
+++ 3764-maximum-sum-with-at-most-k-elements/3764-maximum-sum-with-at-most-k-elements.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long maxSum(vector<vector<int>>& grid, vector<int>& limits, int k) {
+        
+        long long ans=0;
+        int n=grid.size();
+        priority_queue<pair<int,int> > pq;
+        for(int i=0;i<n;i++){
+            sort(grid[i].begin(),grid[i].end());
+            if(limits[i]!=0){
+                cout<<grid[i].back()<<" \n";
+              pq.push({grid[i].back(),i});
+              }
+        }
+        while(k && pq.size()){
+            pair<int,int> tp=pq.top();
+            pq.pop();
+            ans+=(1LL*tp.first);
+            int idx=tp.second;
+            grid[idx].pop_back();
+            limits[idx]--;
+            if(limits[idx]!=0){
+                pq.push({grid[idx].back(),idx});
+            }
+            k--;
+        }
+        return ans;
+    }
+};
